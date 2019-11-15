a5061d6f6fe5ff9ffad9e5e9e877ffdc19696960
Blair Lyons <blair208@gmail.com>
2019-11-14 23:00:05 -0800

    lights fade

--- Assets/Scripts/LightOptimizer.cs
+++ Assets/Scripts/LightOptimizer.cs
@@ -3,7 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class LightOptimizer : MonoBehaviour
-{
+{   
+    public float fadeDuration;
+    [SerializeField]
+    bool fading;
+    
+    float intensity;
+    float t;
+    int fadeDirection;
+    
     Light _light;
     Light theLight
     {
@@ -12,6 +20,7 @@ public class LightOptimizer : MonoBehaviour
             if (_light == null)
             {
                 _light = GetComponent<Light>();
+                intensity = _light.intensity;
             }
             return _light;
         }
@@ -26,10 +35,35 @@ public class LightOptimizer : MonoBehaviour
     void OnTriggerEnter (Collider other)
     {
         theLight.enabled = true;
+        Fade(1);
     }
 
     void OnTriggerExit(Collider other)
     {
         theLight.enabled = false;
+        Fade(-1);
+    }
+
+    void Fade (int _direction)
+    {
+        fadeDirection = _direction;
+        t = _direction > 0 ? 0 : 1f;
+        theLight.intensity = t * intensity;
+        fading = true;
+    }
+
+    void Update()
+    {
+        if (fading)
+        {
+            t += fadeDirection * 1f / fadeDuration * Time.deltaTime;
+            if ((fadeDirection > 0 && t >= 1f) || (fadeDirection < 0 && t <= 0))
+            {
+                t = fadeDirection > 0 ? 1f : 0;
+                fading = false;
+            }
+            
+            theLight.intensity = t * intensity;
+        }
     }
 }
