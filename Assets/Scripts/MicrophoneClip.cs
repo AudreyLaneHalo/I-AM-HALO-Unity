423bfe224e5ac60c6cc9b253b4e6edf63dfd22be
Blair Lyons <blair208@gmail.com>
2019-11-15 17:12:13 -0800

    microphone might work?

--- Assets/Scripts/MicrophoneClip.cs
+++ Assets/Scripts/MicrophoneClip.cs
@@ -1,50 +1,34 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-//using NatMic.Recorders;
 
 [RequireComponent( typeof(AudioSource) )]
 public class MicrophoneClip : MonoBehaviour 
 {
-    //ClipRecorder clipRecorder;
-    //bool recording;
+	bool debug = false;
+	string deviceName = "Built-in Microphone"; //"Living Room Speaker";
 
-    //AudioSource _audioSource;
-    //AudioSource audioSource
-    //{
-    //    get
-    //    {
-    //        if (_audioSource == null)
-    //        {
-    //            _audioSource = GetComponent<AudioSource>();
-    //            Debug.Log(_audioSource == null);
-    //        }
-    //        return _audioSource;
-    //    }
-    //}
+	void Start () 
+	{
+		if (debug)
+		{
+			foreach (string device in Microphone.devices) 
+			{
+				Debug.Log("Audio device available: " + device);
+			}
+		}
 
-    //void Start ()
-    //{
-    //    Debug.Log("start");
-    //    clipRecorder = new ClipRecorder(Play);
-    //    recording = true;
-    //}
+		if (new List<string>( Microphone.devices ).Find( s => s == deviceName ) == null)
+		{
+			Debug.LogWarning( "Can't find " + deviceName + "!" );
+		}
 
-    //void Update ()
-    //{
-    //    if (Time.time > 1 && recording)
-    //    {
-    //        Debug.Log("play " + clipRecorder);
-    //        clipRecorder.Dispose();
-    //        recording = false;
-    //    }
-    //    //clipRecorder = new ClipRecorder(Play);
-    //}
-
-    //void Play (AudioClip _clip)
-    //{
-    //    Debug.Log("started playing " + _clip.samples);
-    //    audioSource.clip = _clip;
-    //    audioSource.Play();
-    //}
+		AudioSource audio = GetComponent<AudioSource>();
+		if (audio != null)
+		{
+			audio.clip = Microphone.Start( deviceName, true, 10, 44100 );
+			audio.loop = true;
+			audio.Play();
+		}
+	}
 }
